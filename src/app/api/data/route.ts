7399e1de3fd491eb59301087394a881a45faaa20
nrjrajpal <nrjrpl007@gmail.com>
2025-02-11 17:09:25 +0530

    fix build issues

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,14 +1,16 @@
 // app/api/data/route.ts
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   // Build the response data
-  const data = { message: 'Hello from Next.js API (Pages Function)!' };
+  console.log(request);
+  const data = { message: "Hello from Next.js API (Pages Function)!" };
   // Return a valid JSON response
   return NextResponse.json(data);
 }
 
 // Optionally, handle other methods if needed
 export async function POST(request: Request) {
-  return new Response('Method Not Allowed', { status: 405 });
+  console.log(request);
+  return new Response("Method Not Allowed", { status: 405 });
 }
